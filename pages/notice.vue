e74f7ea160894718a620e3bb2db351e723ca5416
Crystal <lupuscrystal@proton.me>
2025-06-28 17:01:51 +0000

    Update notice.vue

--- pages/notice.vue
+++ pages/notice.vue
@@ -1,7 +1,7 @@
 <template>
 
     <!-- Message from me from the past to me from the future: Do not forget to add styling of this page, and update it's content when added new assets. -->
-    <div class="w-screen min-h-screen flex flex-col justify-center items-center">
+    <div class="w-screen min-h-screen flex flex-col justify-center items-center text-white">
         <h1 class="font-bold text-3xl md:text-4xl">Notice</h1>
         <ol class="underline font-bold text-2xl md:text-3xl p-2 md:p-4">
             <li><a href="https://unsplash.com/photos/red-raspberries-yp1jDkFCiLs">Raspberries Picture</a></li>
